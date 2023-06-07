e3318399012416eb17bb441a63e6af487a12e3e3
rdayok <r.dayok@native.semicolon.africa>
2023-06-07 21:08:53 +0100

    stack implementation done

--- src/RdStack.java
+++ src/RdStack.java
@@ -1,57 +1,13 @@
 public class RdStack {
 
     private boolean isEmpty;
-    private int elementCounter;
-    private final int stackMemoryBlockSize;
-
-    private final Object[] stackMemoryBlock;
-
-    public RdStack(int stackMemoryBlockSize) {
-        this.stackMemoryBlockSize = stackMemoryBlockSize;
-        stackMemoryBlock = new Object[stackMemoryBlockSize];
-    }
-
-    public <T> void push(T element) {
-        validateStackCapacity();
-        stackMemoryBlock[elementCounter++] = element;
-        tellStackIsEmptyOrNot();
-    }
-
-    public void pop() {
-        elementCounter--;
-        validateStackCapacity();
-        tellStackIsEmptyOrNot();
-    }
-
-    private void tellStackIsEmptyOrNot() {
-        isEmpty = elementCounter == 0;
+    public RdStack(int stackSize) {
     }
 
     public boolean isEmpty() {
         return isEmpty;
     }
 
-    public boolean isFull() {
-        return elementCounter == stackMemoryBlockSize;
-    }
-
-    public int size() {
-        return elementCounter;
-    }
-
-    public Object peek() {
-        int lastElementInStack = elementCounter - 1;
-        return stackMemoryBlock[lastElementInStack];
-    }
-
-    private void validateStackCapacity() {
-        boolean stackIsEmpty = elementCounter < 0;
-        boolean stackIsFull = elementCounter > stackMemoryBlockSize;
-        if (stackIsFull) throwArrayOutOfBoundException("Stack is full and you can't push");
-        if (stackIsEmpty) throwArrayOutOfBoundException("Stack is Empty and you can't pop");
-    }
-
-    private void throwArrayOutOfBoundException(String message) throws IndexOutOfBoundsException{
-        throw new ArrayIndexOutOfBoundsException(message);
+    public void push(String element) {
     }
 }
--- test/RdStackTest.java
+++ test/RdStackTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -7,87 +8,19 @@ public class RdStackTest {
      RdStack rdStack;
     @BeforeEach public void beforeEach(){
         rdStack = new RdStack(5);
-        rdStack.push("retnaa");
     }
 
     @Test public void testStackExist(){
-        assertNotNull(rdStack);
+        Assertions.assertNotNull(rdStack);
     }
 
-    @Test public void testStackIsNotEmptyAfterAPush(){
-        assertFalse(rdStack.isEmpty());
-    }
-
-    @Test public void testStackIsEmpty_whenIPopAfterPushingAnElement(){
-        rdStack.pop();
+    @Test public void testStackIsEmpty(){
         assertTrue(rdStack.isEmpty());
     }
 
-    @Test public void testPushXYZ_stackShouldNotBeEmptyAfterPop(){
-        rdStack.push("segun");
-        rdStack.push("ebuka");
-
-        rdStack.pop();
+    @Test public void testStackIsNotEmptyAfterPushingX(){
+        rdStack.push("element");
 
         assertFalse(rdStack.isEmpty());
     }
-
-    @Test public void testStackIsFullAfterPushingElementsToStackCapacity(){
-        rdStack.push("segun");
-        rdStack.push("ebuka");
-        rdStack.push("oyi");
-        rdStack.push("ashlame");
-
-        assertTrue(rdStack.isFull());
-    }
-
-    @Test public void testStackIsNotFullAfterPushingXY(){
-        rdStack.push("segun");
-
-        assertFalse(rdStack.isFull());
-    }
-    @Test public void testGettingStackSize(){
-        assertEquals(1, rdStack.size());
-    }
-
-    @Test public void testStackSizeAfterPushingZY(){
-        rdStack.push("segun");
-
-        assertEquals(2, rdStack.size());
-    }
-
-    @Test public void testStackPeakMethodToSeeTheLastPushedElement(){
-        assertEquals("retnaa", rdStack.peek());
-    }
-
-    @Test public void testPeakAfterPushingXYZ_elements(){
-        rdStack.push("segun");
-        rdStack.push("oyi");
-
-        assertEquals("oyi", rdStack.peek());
-    }
-
-    @Test public void testPeakAfterAPop(){
-        rdStack.push("segun");
-        rdStack.push("oyi");
-
-        rdStack.pop();
-
-        assertEquals("segun", rdStack.peek());
-    }
-
-    @Test public void testPushingMoreThanStackMemoryBlockCanContain(){
-        rdStack.push("segun");
-        rdStack.push("oyi");
-        rdStack.push("ebuka");
-        rdStack.push("ashlame");
-
-        assertThrows(IndexOutOfBoundsException.class, () -> rdStack.push("dayok"));
-    }
-
-    @Test public void testPopWhenStackIsEmpty(){
-        rdStack.pop();
-        assertThrows(IndexOutOfBoundsException.class, () -> rdStack.pop());
-    }
-
 }
