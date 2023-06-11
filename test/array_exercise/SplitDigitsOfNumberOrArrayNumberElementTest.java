4c873d7e86e28207cc57311dd2c2f1f154af4b90
rdayok <r.dayok@native.semicolon.africa>
2023-06-11 21:42:08 +0100

    Split Digits of Number or array number element refactor of the test class

--- test/array_exercise/SplitDigitsOfNumberOrArrayNumberElementTest.java
+++ test/array_exercise/SplitDigitsOfNumberOrArrayNumberElementTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class SplitDigitsTest {
+public class SplitDigitsOfNumberOrArrayNumberElementTest {
 
     SplitDigitsOfNumberOrArrayNumberElement splitDigitsOfNumber = new SplitDigitsOfNumberOrArrayNumberElement();
 
