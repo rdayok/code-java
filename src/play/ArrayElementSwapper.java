8ea99726ec1a3add1cc5b5cca4269034c44fda86
rdayok <r.dayok@native.semicolon.africa>
2023-09-11 17:31:52 +0100

    refactored my array element swapper

--- src/play/ArrayElementSwapper.java
+++ src/play/ArrayElementSwapper.java
@@ -2,17 +2,10 @@ package play;
 
 public class ArrayElementSwapper {
 
-    private int[] inputtedArray;
-    private boolean elementAtIndexA_isNegative;
-    private boolean elementAtIndexB_isNegative;
-    private final int ZERO = 0;
-    private final int ONE = 1;
-
-
 
     public int[] swapElementsFor(int[] inputtedArray, int indexA, int indexB) {
         final int ARRAY_LAST_INDEX = inputtedArray.length;
-        this.inputtedArray = inputtedArray;
+        int ZERO = 0;
         boolean isInvalidIndexForGivenArray = indexA == indexB || indexA >= ARRAY_LAST_INDEX || indexB >= ARRAY_LAST_INDEX || indexA < ZERO || indexB < ZERO;
         if (isInvalidIndexForGivenArray) return inputtedArray;
         performSwappingLogic(inputtedArray, indexA, indexB);
@@ -20,24 +13,9 @@ public class ArrayElementSwapper {
     }
 
     private void performSwappingLogic(int[] inputtedArray, int indexA, int indexB) {
-        isNegativeElementAt(indexA, "indexA");
-        isNegativeElementAt(indexB, "indexB");
         inputtedArray[indexA] += inputtedArray[indexB];
         inputtedArray[indexB] = inputtedArray[indexA] - inputtedArray[indexB];
         inputtedArray[indexA] -= inputtedArray[indexB];
-        changeSymbolForElementsAt(indexA, indexB);
     }
 
-    private void changeSymbolForElementsAt(int indexA, int indexB) {
-        if(elementAtIndexA_isNegative)  inputtedArray[indexB] *= -ONE;
-        if(elementAtIndexB_isNegative) inputtedArray[indexA] *= -ONE;
-    }
-
-    private void isNegativeElementAt(int givenIndex, String index) {
-        if(inputtedArray[givenIndex] < ZERO){
-            if(index.equals("indexA")) elementAtIndexA_isNegative = true;
-            if(index.equals("indexB")) elementAtIndexB_isNegative = true;
-            inputtedArray[givenIndex] *= -ONE;
-        }
-    }
 }
